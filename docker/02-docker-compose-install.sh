e858e0c9fbd0073dd2e8f777cacefc28fc97ca5e
Andrey Mishchenko <msav@msav.ru>
2021-07-03 19:36:07 +0300

    Added the docker-compose installation script.

---
+++ docker/02-docker-compose-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Download the current stable release of Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
